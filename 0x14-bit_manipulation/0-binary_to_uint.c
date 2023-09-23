8c3241359a33d9f18e175d697aab294fba9f8d37
KibeO <kibet.owiti@gmail.com>
2023-09-23 13:45:33 -0700

    binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,39 @@
+#include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * binary_to_uint - converts binary number to unsigned int
+ * @b: string
+ * Return: Converted number || 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	int slen = 0, i;
+	unsigned int total = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (b[slen] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		slen++;
 	}
+	slen -= 1;
+	i = 0;
 
-	return (num);
+	while (b[i])
+	{
+		if ((b[i] != '0') && (b[i] != '1'))
+		{
+			return (total);
+		}
+		if (b[i] == '1')
+		{
+			total += (1 * (1 << slen));
+		}
+		i++;
+		slen--;
+	}
+	return (total);
 }
